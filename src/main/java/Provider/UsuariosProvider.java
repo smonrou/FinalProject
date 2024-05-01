b03661705727fef57a92938043b6a54aeb1d89fd
smonrou <saulnadremm@gmail.com>
2024-05-01 16:20:44 -0600

    UsuariosProvider works

---
+++ src/main/java/Provider/UsuariosProvider.java
@@ -0,0 +1,27 @@
+package Provider;
+
+import DataClass.Usuario;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class UsuariosProvider {
+    private static final List<Usuario> userList = new ArrayList<>();
+    
+    public static void addUsuario(Usuario user){
+        userList.add(user);
+    }
+    
+    public static Usuario getUserByID(Usuario users){
+        Usuario result = new Usuario();
+        for(Usuario user: userList){
+            if(users.getId() == user.getId()){
+                result = user;
+            }
+        }
+        return result;
+    }
+}
